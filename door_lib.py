3e9ef2c146e8b9fcce74bea751d6b71370c30cb6
root <root@door.fd>
2019-06-02 18:38:50 +0200

    more logs

--- door_lib.py
+++ door_lib.py
@@ -1,25 +1,28 @@
 import contextlib
 import logging
+from systemd.journal import JournalHandler
 
 import requests
 import serial
+from time import sleep
 
 import config
 
 base_url = "https://api.flipdot.org/sensors/door/locked/"
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(JournalHandler())
 
 def close():
     with get_serial() as s:
         try:
             if is_open(s):
                 s.write('0')
-                update_api(locked=True)
                 return True
             else:
-                update_api(locked=True)
                 return False
         except Exception as e:
-            print(e)
+            logger.exception("close")
             pass
 
 def open():
@@ -27,26 +30,27 @@ def open():
         try:
             if not is_open(s):
                 s.write('1')
-                update_api(locked=False)
                 return True
             else:
-                update_api(locked=False)
                 return False
         except Exception as e:
-            print(e)
+            logger.exception("open")
             pass
 
 
 def update_api(locked=None):
+    logger.debug("update api")
     if locked is None:
         with get_serial() as s:
             locked = not is_open(s)
+            logger.debug("door is {}".format(locked))
 
     value = 1 if locked else 0
+    logger.debug("update api to {}".format(value))
     try:
-        print("posted api: ", requests.get(base_url + str(value), timeout=3).content)
+        logger.debug("posted api: {}".format(requests.get(base_url + str(value), timeout=3).content))
     except Exception as e:
-        print("error posting status", repr(e))
+        logger.exception("error posting api status")
 
 
 def get_serial():
@@ -54,18 +58,22 @@ def get_serial():
     return contextlib.closing(s)
 
 def get_state(s):
-    line = s.readline()
-    logging.debug("line: {}".format(line))
-    first = line.split(" ")[0]
-    try:
-        adc = int(first)
-    except ValueError:
-        msg = "ValueError: '%s' is not a valid door state" % first
-        logging.error(msg)
-        print(msg)
-        return False, first
-    logging.debug("adc: {}".format(adc))
-    return adc > 500, adc
+    for i in range(5):
+        line = s.readline()
+        logger.debug("line: {}".format(line))
+        if not line or len(line) < 3:
+            continue
+        first = line.split(" ")[0]
+        try:
+            adc = int(first)
+        except ValueError:
+            msg = "ValueError: '{}' is not a valid door state.".format(first)
+            logger.error(msg)
+            sleep(1)
+            continue
+        logger.debug("adc: {}".format(adc))
+        return adc > 500, adc
+    return None, "unk"
 
 def is_open(s):
     door_is_open, _ = get_state(s)
